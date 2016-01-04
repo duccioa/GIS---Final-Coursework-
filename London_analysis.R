ad2e707a75092108c59ba133b52879f3e1256285
duccioa <duccio.aiazzi@gmail.com>
2016-01-04 18:14:53 +0000

    Reload

--- London_analysis.R
+++ London_analysis.R
@@ -14,14 +14,37 @@ london_pol40 <- readOGR("../shapes/london/clean", "london_clean_40")
 plot(london_pol40, col = "red", border = "white")
 plot(london_boundaries, col = "black", add = TRUE)
 
-london_pol40 <- SPDF.Cmi_Index(london_pol40)#Calculate compactness
-london_pol40@data$C_mi <- abs(london_pol40@data$C_mi)
-
-alpha_col <- add.alpha("blue", 0.5)
-plot(london_pol40@data$id, london_pol40@data$C_mi, 
-     ylim = c(-0.000005,0.000005), col = alpha_col, pch = 20, cex = 1)
-
 
 #debug of the analysis
+##NEGATIVE VALUES
 areas <- extract.areas(london_pol40)
 london_Cmi <- SPDF.Cmi_Index(london_pol40)
+negative_values <- london_Cmi[london_Cmi@data$C_mi < 0,]
+plot(negative_values, lwd = .01, border = "cornsilk3", col = "red")
+areas_negative <- extract.areas(negative_values)
+coords_neg_id15 <- london_Cmi@polygons[15][[1]]@Polygons[[1]]@coords
+Sr1 <- Polygon(coords_neg_id15)
+Sr1 <- Polygons(list(Sr1), "s1")
+Sr1 <- SpatialPolygons(list(Sr1), 1:1)
+spdf <- SpatialPolygonsDataFrame(Sr1,
+                                 data=data.frame(x=1, y=1, row.names=row.names(Sr1)))
+plot(spdf)
+Polygon.Cmi_index(spdf)###It looks like the polygon is different from the one fetched subsetting the map
+spdf <- london_Cmi[london_Cmi@data$id == 15,]
+##INFINITE VALUES
+inf_values <- london_Cmi[london_Cmi@data$C_mi == Inf,]
+plot(inf_values, lwd = .01, border = "cornsilk3", col = "blue")
+head(inf_values@data, 30)
+inf_values@polygons[90][[1]]@Polygons[[1]]@coords
+areas_inf <- extract.areas(inf_values)
+hist(areas_inf, breaks = 50)
+summary(areas_inf)
+
+
+#Intermediate values
+int_values <- london_Cmi[london_Cmi@data$C_mi != Inf & london_Cmi@data$C_mi >= 0,]
+summary(int_values@data$C_mi)
+int_values <- int_values[int_values@data$C_mi < 1,]
+hist(int_values@data$C_mi, breaks = 100)
+plot_C_mi(int_values, plot_variable = "C_mi", n = 8, col_pal = "YlGnBu", style_intervals = "quantile")
+plot_C_mi(int_values, plot_variable = "C_mi", n = 4, col_pal = "YlGnBu", style_intervals = "quantile")
--- Paris_analysis.R
+++ Paris_analysis.R
@@ -10,22 +10,18 @@ add.alpha <- function(col, alpha=1){
           function(x) 
               rgb(x[1], x[2], x[3], alpha=alpha))  
 }
-paris_pol40 <- readOGR("./shapes/paris/clean", "paris_clean_40")
+paris_pol40 <- readOGR("../shapes/paris/clean", "paris_clean_40")
 paris_boundaries <- readOGR("./shapes/paris/boundaries", "paris_boundaries")
 plot(paris_pol40, col = "red", border = "white")
 plot(paris_boundaries, col = "black", add = TRUE)
 
-alpha_col <- add.alpha("red", 0.5)
 paris_cmi <- SPDF.Cmi_Index(paris_pol40)#Calculate compactness
-#Analysis of the sign
-paris_cmi@data$col <- "green"
-upper_limit <- 0.98
-lower_limit <- 0.02
-paris_cmi@data$col[paris_cmi@data$C_mi >= quantile(paris_cmi@data$C_mi, upper_limit)] <- "blue"
-paris_cmi@data$col[paris_cmi@data$C_mi <= quantile(paris_cmi@data$C_mi, lower_limit)] <- "yellow"
-paris_cmi@data$col[paris_cmi@data$C_mi>0] <- "red"
-plot(paris_cmi, col = paris_cmi@data$col)
-
+summary(paris_cmi@data$C_mi)
+paris_int_values <- paris_cmi[paris_cmi@data$C_mi != Inf & paris_cmi@data$C_mi >= 0,]
+summary(paris_int_values@data$C_mi)
+paris_int_values <- paris_int_values[paris_int_values@data$C_mi < 1,]
+hist(paris_int_values@data$C_mi, breaks = 100)
+plot_C_mi(paris_int_values, plot_variable = "C_mi", n = 4, col_pal = "YlGnBu", style_intervals = "quantile")
 
 
 
